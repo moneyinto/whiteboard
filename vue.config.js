92927adbcec58384a588c099883116e8b565ae67
moneyinto <woyaoqianjin@gmail.com>
2023-03-21 18:14:28 +0800

    移除serviceWorker 修改图标

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
     productionSourceMap: false,
-    configureWebpack: {
-        devtool: "source-map"
-    },
     devServer: {
         host: "localhost",
         open: true
